900056ac257c1b54159940dc20b6ef6a4aa9bb88
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-08-20 17:42:22 +0300

    Add dropout to each layer of the osnet

--- scripts/convert_to_onnx.py
+++ scripts/convert_to_onnx.py
@@ -1,27 +1,13 @@
-import sys
-import os
-import os.path as osp
-import warnings
-import time
-
 import torch
-import torch.nn as nn
+import numpy as np
+from PIL import Image
 
-from default_parser import (
-    init_parser, imagedata_kwargs, videodata_kwargs,
-    optimizer_kwargs, lr_scheduler_kwargs, engine_run_kwargs
-)
+from default_parser import init_parser
 from torchreid.data.transforms import build_transforms
 import torchreid
-from torchreid.utils import (
-    Logger, set_random_seed, check_isfile, resume_from_checkpoint,
-    load_pretrained_weights, compute_model_complexity, collect_env_info
-)
+from torchreid.utils import load_pretrained_weights
 
 
-import glog as log
-import numpy as np
-import cv2 as cv
 
 
 parser = init_parser()
@@ -41,13 +27,11 @@ model.eval()
 
 _, transform = build_transforms(args.height, args.width)
 
-input_size=(args.height, args.width, 3)
+input_size = (args.height, args.width, 3)
 img = np.random.rand(*input_size).astype(np.float32)
 img = np.uint8(img*255)
-from PIL import Image
 im = Image.fromarray(img)
 blob = transform(im).unsqueeze(0)
-
 
 torch.onnx.export(model, blob, args.output_name + '.onnx',
                   verbose=True, export_params=True)
