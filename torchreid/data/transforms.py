ea441b0e32fd3f974e962101b8437f22ff07ba5e
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-10-22 17:14:02 +0300

    Decrease stranght of affine augm

--- torchreid/data/transforms.py
+++ torchreid/data/transforms.py
@@ -257,7 +257,7 @@ def build_transforms(height, width, transforms='random_flip', norm_mean=[0.485, 
         transform_tr += [ColorJitter(brightness=0.2, contrast=0.15, saturation=0, hue=0)]
     if 'random_affine' in transforms:
         print('+ random affine')
-        transform_tr += [RandomAffine(10, translate=(0.1, 0.1), scale=(0.9, 1.1))]
+        transform_tr += [RandomAffine(5, translate=(0.05, 0.05), scale=(0.95, 1.15))]
     print('+ to torch tensor of range [0, 1]')
     transform_tr += [ToTensor()]
     print('+ normalization (mean={}, std={})'.format(norm_mean, norm_std))
