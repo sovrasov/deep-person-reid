05e224ce87544c8fb8e420a0256bfc4f47fb995a
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-10-23 15:01:33 +0300

    Update onnx converion script

--- scripts/convert_to_onnx.py
+++ scripts/convert_to_onnx.py
@@ -1,37 +1,54 @@
+import argparse
+
 import torch
 import numpy as np
 from PIL import Image
 
-from deprecated.default_parser import init_parser
 from torchreid.data.transforms import build_transforms
 import torchreid
 from torchreid.utils import load_pretrained_weights
 
+from default_config import get_default_config
 
 
+def main():
 
-parser = init_parser()
-parser.add_argument('--output_name', type=str, default='model')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config-file', type=str, default='', help='path to config file')
+    parser.add_argument('--output_name', type=str, default='model')
+    parser.add_argument('opts', default=None, nargs=argparse.REMAINDER, help='Modify config options using the command-line')
+    args = parser.parse_args()
 
-model = torchreid.models.build_model(
-        name=args.arch,
-        num_classes=1041,
-        loss=args.loss.lower(),
-        pretrained=(not args.no_pretrained),
-        use_gpu=True,
-        feature_dim=args.feature_dim
-    )
-load_pretrained_weights(model, args.load_weights)
-model.eval()
+    cfg = get_default_config()
+    cfg.use_gpu = torch.cuda.is_available()
+    if args.config_file:
+        cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
 
-_, transform = build_transforms(args.height, args.width)
+    model = torchreid.models.build_model(
+        name=cfg.model.name,
+        num_classes=2,
+        loss=cfg.loss.name,
+        pretrained=cfg.model.pretrained,
+        use_gpu=cfg.use_gpu,
+        dropout_prob=cfg.model.dropout_prob,
+        feature_dim=cfg.model.feature_dim,
+        activation=cfg.model.activation
+        )
+    load_pretrained_weights(model, cfg.model.load_weights)
+    model.eval()
 
-input_size = (args.height, args.width, 3)
-img = np.random.rand(*input_size).astype(np.float32)
-img = np.uint8(img*255)
-im = Image.fromarray(img)
-blob = transform(im).unsqueeze(0)
+    _, transform = build_transforms(cfg.data.height, cfg.data.width)
 
-torch.onnx.export(model, blob, args.output_name + '.onnx',
-                  verbose=True, export_params=True)
+    input_size = (cfg.data.height, cfg.data.width, 3)
+    img = np.random.rand(*input_size).astype(np.float32)
+    img = np.uint8(img*255)
+    im = Image.fromarray(img)
+    blob = transform(im).unsqueeze(0)
+
+    torch.onnx.export(model, blob, args.output_name + '.onnx',
+                      verbose=True, export_params=True)
+
+
+if __name__ == '__main__':
+    main()
